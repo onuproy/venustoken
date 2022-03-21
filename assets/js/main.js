c62663ef3ca84c07c2ec8b91756e22e044e17b9f
Onup Chandra Barmon <anupme01@gmail.com>
2022-03-21 23:27:56 +0600

    title update

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function(){ 
+
+	// Mobile menu
+	$('.nav-icon').click(function () {
+		$('.menu').toggleClass('canvas-menu');
+		return false;
+
+	});
+
+	$('.menu ul li a').click(function () {
+		$('.menu').toggleClass('canvas-menu');
+
+	});
+
+	
+
+		
+		
+	
+
+});
